c4d31a8c9c407e63ab84bd9d7c080da140bf51a4
rizalyneasaldo1 <rizalyne.ayen03@gmail.com>
2024-10-26 00:04:02 +0800

    merging hardway into main and finalizing personal-portfolio

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -23,21 +23,21 @@
   /**
    * Navbar effects and scrolltop buttons upon scrolling
    */
-  const navbar = document.getElementById('header-nav')
+  const navbar = document.getElementById('header-nav');
   var body = document.getElementsByTagName("body")[0]
   const scrollTop = document.getElementById('scrolltop')
   window.onscroll = () => {
     if (window.scrollY > 0) {
-      navbar.classList.add('fixed-top', 'shadow-sm')
-      body.style.paddingTop = navbar.offsetHeight + "px"
+      navbar.classList.add('fixed-top', 'shadow-sm');
+      body.style.paddingTop = navbar.offsetHeight + "px";
       scrollTop.style.visibility = "visible";
       scrollTop.style.opacity = 1;
-    } else {
-      navbar.classList.remove('fixed-top', 'shadow-sm')
-      body.style.paddingTop = "0px"
+  } else {
+      navbar.classList.remove('fixed-top', 'shadow-sm');
+      body.style.paddingTop = "0px";
       scrollTop.style.visibility = "hidden";
       scrollTop.style.opacity = 0;
-    }
+  }
   };
 
   /**
